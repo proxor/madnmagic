dfda3d010338e7da4d1d996e7355461af7107c4f
Prokhor <p@shuchalov.ru>
2010-03-13 23:48:55 +0500

    Products add/edit/view/del

--- application/controllers/products.php
+++ application/controllers/products.php
@@ -5,21 +5,63 @@ class Products_Controller extends Controller {
 	{
 		$cats = new Category_Model();
 		$cats = $cats->find_all();
+		
+		echo "<ul>";
 		foreach($cats as $c) {
-			echo "<li>$c->title</li>";
+			echo "<li>$c->title";
+			if(count($c->products)) {
+				echo "<ul>";
+				foreach($c->products as $p) {
+					echo "<li>$p->title</li>";
+				}
+				echo "</ul>";
+			}
+			echo "</li>";
 		}	
+		echo "</ul>";
 	}
 
 	public function edit($id = 0) {
-		print form::open(NULL,array('method' => 'post'));
-		print form::label('title','Название'); 
-		print form::text('title'); 
-		print form::label('title','Описание'); 
+		$post = new Validation($_POST);	
+		$post->add_rules('title','required');
+		#$post->add_rules('category','required');
+	
+		$product = new Product_Model($id);
+		
+		if($post->validate()) {
+			$product = new Product_Model($post->id);
+			$product->title = $post->title;
+			$product->description = $post->description;
+			$product->category_id = $post->category;
+			$product->save();
+			$product->reload();
+		}
+		elseif($post->errors()) {
+			print_r($post->errors());
+		}
+
+		$cats = new Category_Model();
+		$cats = $cats->find_all();
+		$cats_select = array();
+		foreach($cats as $c) {
+			$cats_select[$c->id] = $c->title;
+		}	
+
+		print form::open(NULL,array('method' => 'post'),array('id' => $product->id));
+		print form::label('category','Категория:')."<br/>"; 
+		print form::dropdown('category',$cats_select,$product->category)."<br/>"; 
+		print form::label('title','Название:')."<br/>"; 
+		print form::input('title',$product->title)."<br/>"; 
+		print form::label('description','Описание:')."<br/>"; 
+		print form::textarea('description',$product->description)."<br/>"; 
+		print form::submit('submit','Сохранить')."<br/>"; 
 		print form::close();
 	}
 
-	function del() {
-		echo "Error: not yet implemented";
+	function del($id = 0) {
+		$product = new Product_Model($id);
+		if($product)
+			$product->delete();
 	}
 	
 } 
--- application/models/product.php
+++ application/models/product.php
@@ -1,5 +1,5 @@
 <?php
 class Product_Model extends ORM {
-	protected $belongs_to = array('categories');
+	protected $belongs_to = array('category');
 }
 ?>
